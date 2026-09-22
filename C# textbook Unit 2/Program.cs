ceeceaffc917e67111b2c617327be6f079db8065
hoonji <dvbhffvh@gmail.com>
2026-09-23 02:26:52 +0900

    C# 교과서 2단원 내용정리

---
+++ C# textbook Unit 2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace C__textbook_Unit_2
+{
+    internal class Program
+    {
+        /*
+            변수 : 데이터를 메모리에 잠시 보관해 놓고 사용할 수 있는 임시 저장 공간이 필요
+            변수 선언 : 데이터 형식 변수 이름 문장의 끝
+                        int      number    ;
+            데이터 형식      설명
+            int             정수형 데이터 더 큰 정수는 long사용
+            srting          문자열 데이터
+            bool            논리형 데이터
+            double          실수형 데이터
+            object          C#에서 사용하는 모든 데이터
+            
+
+         
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
